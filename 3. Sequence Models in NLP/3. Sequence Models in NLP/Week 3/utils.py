19b48430069dbdad3e5032c61c961208e6136284
minhmnq <24022406@vnu.edu.vn>
2026-08-09 01:13:55 +0700

    Complete all assignment notebook implementations and populate outputs

--- 3. Sequence Models in NLP/3. Sequence Models in NLP/Week 3/utils.py
+++ 3. Sequence Models in NLP/3. Sequence Models in NLP/Week 3/utils.py
@@ -1,12 +1,12 @@
 def get_vocab(vocab_path, tags_path):
     vocab = {}
-    with open(vocab_path) as f:
+    with open(vocab_path, encoding='utf-8', errors='ignore') as f:
         for i, l in enumerate(f.read().splitlines()):
             vocab[l] = i  # to avoid the 0
         # loading tags (we require this to map tags to their indices)
     vocab['<PAD>'] = len(vocab) # 35180
     tag_map = {}
-    with open(tags_path) as f:
+    with open(tags_path, encoding='utf-8', errors='ignore') as f:
         for i, t in enumerate(f.read().splitlines()):
             tag_map[t] = i 
     
@@ -16,7 +16,7 @@ def get_params(vocab, tag_map, sentences_file, labels_file):
     sentences = []
     labels = []
 
-    with open(sentences_file) as f:
+    with open(sentences_file, encoding='utf-8', errors='ignore') as f:
         for sentence in f.read().splitlines():
             # replace each token by its index if it is in vocab
             # else use index of UNK_WORD
@@ -25,7 +25,7 @@ def get_params(vocab, tag_map, sentences_file, labels_file):
                  for token in sentence.split(' ')]
             sentences.append(s)
 
-    with open(labels_file) as f:
+    with open(labels_file, encoding='utf-8', errors='ignore') as f:
         for sentence in f.read().splitlines():
             # replace each label by its index
             l = [tag_map[label] for label in sentence.split(' ')] # I added plus 1 here
